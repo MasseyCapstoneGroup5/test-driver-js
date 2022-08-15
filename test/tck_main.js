e0a34a8d0791b26edf34f279e9e37f2e038b5788
Holly <71049594+HDuCoing@users.noreply.github.com>
2022-08-15 20:45:23 +1200

    main test setup/teardown

--- test/tck_main.js
+++ test/tck_main.js
@@ -1,13 +1,39 @@
+import {JSONRPClient} from "../client.js";
+import {Client, AccountInfo} from "@hashgraph/sdk";
+import {expect} from "chai";
+// mocha --file "./test/setup.js" "./test/**/*.spec.js"
+//setup
+before(function() {
+    /**
+     * Very basic test with hardcoded values. Setup doesn't need to be run
+     */
+    describe('#generatePublicKey()', function () {
+        it('should return the correct public key', async function () {
+            let response = await JSONRPClient.request("generatePublicKey", {
+                    "privateKey": "302e020100300506032b657004220420c036915d924e5b517fae86ce34d8c76005cb5099798a37a137831ff5e3dc0622 "
+                }
+            )
+            expect(response).to.equal('302a300506032b657003210008530ea4b75f639032eda3c18f41a296cf631d1828697e4f052297553139f347');
+        });
+    });
 
-// Language agnostic testing
-async function convertToJS(sdkMethod){
-    // input the SDK
-    // find out language and convert to javascript object
-    try {
-        const sdk_data = JSON.parse(sdk);
-    } catch (error) {
-    //if data is not processed - throw an error.
-        console.log(error);
-    }
-    // return converted SDK
-}
+    /**
+     * Test Create account and compare results with js SDK
+     */
+    describe('#createAccount()', function () {
+
+        beforeEach(async function () {
+            await JSONRPClient.request("setup", {
+                    "operatorAccountId": process.env.OPERATOR_ACCOUNT_ID,
+                    "operatorPrivateKey": process.env.OPERATOR_ACCOUNT_PRIVATE_KEY
+                }
+            )
+        });
+    });
+});
+
+//teardown
+after(function() {
+    //todo: teardown function
+});
+
