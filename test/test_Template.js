59e5b16465d4985fc29461b2d6ba5816f2e766e9
Fabian Toms <tomsfabian@gmail.com>
2022-08-21 18:39:16 +0200

    Add test template

---
+++ test/test_Template.js
@@ -0,0 +1,51 @@
+import {JSONRPClient} from "../client.js";
+import {Client} from "@hashgraph/sdk";
+import {expect} from "chai";
+
+/**
+ * Explain what this test suite is for here
+ */
+describe('#functionalityWeWantToTest()', function () { // a suite of tests
+    this.timeout(10000); // Timeout for all tests and hooks within this suite
+
+    // before and after hooks (normally used to set up and reset the client SDK)
+    before(async function () {
+        await JSONRPClient.request("setup", {
+                "operatorAccountId": process.env.OPERATOR_ACCOUNT_ID,
+                "operatorPrivateKey": process.env.OPERATOR_ACCOUNT_PRIVATE_KEY
+            }
+        )
+    });
+    after(async function () {
+        await JSONRPClient.request("reset")
+    });
+
+    // Before/after each test can also be used
+    beforeEach(function () {});
+    afterEach(function () {});
+
+
+    // Test
+    it('should do something', async function () {
+
+        // Call JSON-RPC (Make sure it is running first)
+        /*let result = await JSONRPClient.request("doSomething", {
+            "parameter": "value"
+        })*/
+
+
+        // Get value of something using this Client (Don't use JSON-RPC)
+        const SDKClient = Client.forTestnet();
+        SDKClient.setOperator(process.env.OPERATOR_ACCOUNT_ID, process.env.OPERATOR_ACCOUNT_PRIVATE_KEY);
+        //const something = await new SomethingQuery.execute(SDKClient);
+        const something = "value";
+
+        // Check if something was successfully completed
+        expect(something).to.equal("value");
+    })
+
+    // Another test in the same suite
+    it('should do something else', async function () {
+
+    })
+});
