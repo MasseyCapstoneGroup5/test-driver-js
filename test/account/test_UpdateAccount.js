c92f710565152b4af3a7f04898a712cc6160c2b7
peter <peter.whyte@xtra.co.nz>
2022-08-24 21:14:32 +1200

    align file structure to test template and add tests

--- test/account/test_UpdateAccount.js
+++ test/account/test_UpdateAccount.js
@@ -1,12 +1,15 @@
 import {JSONRPClient} from "../../client.js";
-import {expect} from "chai";
+import {Client, AccountInfoQuery } from "@hashgraph/sdk";
+import {expect, assert} from "chai";
 
+let newRandomMemo;
 /**
  * Test update account and compare results with js SDK
  */
-describe('#updateAccount()', function () {
-    this.timeout(10000);
+ describe('#updateAccount()', function () { 
+    this.timeout(10000); 
 
+    // before and after hooks (normally used to set up and reset the client SDK)
     before(async function () {
         await JSONRPClient.request("setup", {
                 "operatorAccountId": process.env.OPERATOR_ACCOUNT_ID,
@@ -18,27 +21,48 @@ describe('#updateAccount()', function () {
         await JSONRPClient.request("reset")
     });
 
-
-    it('should update memo for account', async function () {
-
-        let newRandomMemo = Math.random().toString(36).slice(-5);
+    // Test
+    it('should update memo on an account', async function () {
+        // generate a memo of five random char / nums
+        newRandomMemo = Math.random().toString(36).slice(-5);
+        // console.log(newRandomMemo);
         // TODO optional create new account without a memo instead of using a random memo value
 
-        // UpdateAccount with the JSON-RPC
+        // add a memo to the AccountInfo
         await JSONRPClient.request("updateAccount", {
             "accountId": process.env.OPERATOR_ACCOUNT_ID,
             "key": process.env.OPERATOR_ACCOUNT_PRIVATE_KEY,
-            // add a memo to the AccountInfo
             "memo": newRandomMemo
-        });
+        })
+
         // Check if memo has changed using the JS SDK Client
-        let accountInfo = await JSONRPClient.request("getAccountInfo", {
-                "accountId": process.env.OPERATOR_ACCOUNT_ID
-            })
-        ;
-        //console.log(accountInfo.accountMemo);
-        expect(accountInfo.accountMemo).to.equal(newRandomMemo);
+        const SDKClient = Client.forTestnet();
+        SDKClient.setOperator(process.env.OPERATOR_ACCOUNT_ID, process.env.OPERATOR_ACCOUNT_PRIVATE_KEY);
+        const getAccountInf = await new AccountInfoQuery()
+        .setAccountId(process.env.OPERATOR_ACCOUNT_ID)
+        .execute(SDKClient);   
+
+        // Check if memo field was successfully updated
+        expect(getAccountInf.accountMemo).to.equal(newRandomMemo);
     })
 
-
+    // Another test in the same suite
+    it('should return true', async function () {
+        let emptyMemoStr = '';
+        assert.notEqual(newRandomMemo, emptyMemoStr);
+    })
+    it('should return false', async function () {
+        // generate a memo of four random char / nums
+        let testMemoStr4 = Math.random().toString(36).slice(-4);
+        assert.equal(newRandomMemo, testMemoStr4);
+    })
+    it('should return false', async function () {
+        // generate a memo of six random char / nums
+        let testMemoStr6 = Math.random().toString(36).slice(-6);
+        assert.strictEqual(newRandomMemo, testMemoStr6);
+    })
+    it('should return false', async function () {
+        let testMemoNum = 10;
+        assert.equal(newRandomMemo, testMemoNum);
+    })
 });
