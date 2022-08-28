452381dcec0699c8275995ffa38d73b773183324
peter <peter.whyte@xtra.co.nz>
2022-08-29 11:58:32 +1200

    Add single Testnetd "call" for all functions

--- testnetEnquiry.js
+++ testnetEnquiry.js
@@ -1,23 +1,24 @@
 import {Client, AccountInfoQuery, AccountBalanceQuery} from "@hashgraph/sdk";
 
 export async function getBalanceFromTestnet(accountID) {
-    // Use the JS SDK Client to retrieve account balance
-    const SDKClient = Client.forTestnet();
-    SDKClient.setOperator(process.env.OPERATOR_ACCOUNT_ID, process.env.OPERATOR_ACCOUNT_PRIVATE_KEY);
-    const accountBalance = new AccountBalanceQuery()
-    .setAccountId(accountID)
-    .execute(SDKClient);   
+    const accountBalance = getTestnet(accountID, new AccountBalanceQuery())
 
     return accountBalance;    
 };
 
 export async function getInfoFromTestnet(accountID) {
-    // Use the JS SDK Client to retrieve account information
-    const SDKClient = Client.forTestnet();
-    SDKClient.setOperator(process.env.OPERATOR_ACCOUNT_ID, process.env.OPERATOR_ACCOUNT_PRIVATE_KEY);
-    const accountInfo = new AccountInfoQuery()
-    .setAccountId(accountID)
-    .execute(SDKClient);   
+    const accountInfo = getTestnet(accountID, new AccountInfoQuery()) 
 
     return accountInfo;    
 };
+
+async function getTestnet(accountID, method) {
+    // Use the JS SDK Client to retrieve account information
+    const SDKClient = Client.forTestnet();
+    SDKClient.setOperator(process.env.OPERATOR_ACCOUNT_ID, process.env.OPERATOR_ACCOUNT_PRIVATE_KEY);
+    const query = await method
+    .setAccountId(accountID)
+    .execute(SDKClient);   
+
+    return query;
+}
