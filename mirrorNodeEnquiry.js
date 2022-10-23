180b1a348b394dd09e44869781662cabc5b75aa6
peter <peter.whyte@xtra.co.nz>
2022-10-23 22:33:09 +1300

    add a different API call for account bal from mirror node

--- mirrorNodeEnquiry.js
+++ mirrorNodeEnquiry.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-export async function getJsonData(accountid) {
+export async function getJsonAccountData(accountid) {
     /**
      * Allow a set period of time (max 10s) for data to be added or updated on the Hedera network
      * before querying the Hedera mirror node to check and verify the data 
@@ -10,6 +10,18 @@ export async function getJsonData(accountid) {
     const response = await fetch(url)
     return await response.json()
 }
-  function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time))
+
+export async function getJsonBalanceData(accountid) {
+  /**
+   * Allow a set period of time (max 10s) for data to be added or updated on the Hedera network
+   * before querying the Hedera mirror node to check and verify the data 
+  **/
+  await delay(process.env.NODE_DELAY)
+  let url = `${process.env.MIRROR_NODE_REST_URL}/api/v1/balances?account.id=${accountid}`
+  const response = await fetch(url)
+  return await response.json()
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time))
 }
--- test/account/test_CreateAccount.js
+++ test/account/test_CreateAccount.js
@@ -1,6 +1,6 @@
 import { JSONRPCRequest } from '../../client.js'
 import { getAccountInfo } from '../../SDKEnquiry.js'
-import { getJsonData } from '../../mirrorNodeEnquiry.js'
+import { getJsonAccountData, getJsonBalanceData } from '../../mirrorNodeEnquiry.js'
 import { 
   generateAccountKeys,
   setOperator,
@@ -45,7 +45,7 @@ describe('#createAccount()', function () {
       const accountIDFromConsensusNode = accountInfoFromConsensusNode.accountId.toString()
 
       // query account via mirror node to confirm availability after creation
-      const accountInfoFromMirrorNode = await getJsonData(accountIDFromConsensusNode)
+      const accountInfoFromMirrorNode = await getJsonAccountData(accountIDFromConsensusNode)
       const accountIDFromMirrorNode = accountInfoFromMirrorNode.accounts[0].account
 
       // confirm pass status with assertion testing for account creation
@@ -90,8 +90,10 @@ describe('#createAccount()', function () {
         return
       }
       assert.fail("Should throw an error")
-    })    
-    //----------- Create an account with an initial balance -----------
+    })
+  })  
+
+  //----------- Create an account with an initial balance -----------
   describe('Create an account with an initial balance', function () {
     it('Sets initial balance to 100 HBar', async function () {
       // set initial balance of 10,000,000,000 tinybars (100 Hbar) 
@@ -112,13 +114,13 @@ describe('#createAccount()', function () {
       const accountBalanceConsensusNode = accountBalConsensus._valueInTinybar
 
       // query account balance via mirror node to check amount set
-      const accountInfoFromMirrorNode = await getJsonData(accountIDFromConsensusNode)
-      const accountBalMirror = accountInfoFromMirrorNode.accounts[0].balance
-      const accountBalanceMirrorNode = accountBalMirror._valueInTinybar
+      const accountInfoFromMirrorNode = 
+        await getJsonBalanceData(accountIDFromConsensusNode)
+      const accountBalanceMirrorNode = 
+        accountInfoFromMirrorNode.balances[0].balance
 
-      // confirm pass status with assertion testing for account creation
       expect(initialBal).to.equal(Number(accountBalanceConsensusNode))
-      //expect(initialBal).to.equal(Number(accountBalanceMirrorNode))
+      expect(initialBal).to.equal(Number(accountBalanceMirrorNode))
     })
     // Set initial balance to -1 HBAR
     it('Sets initial balance to -1 HBar', async function () {
@@ -183,10 +185,10 @@ describe('#createAccount()', function () {
         );
         return
       }
-      assert.fail("Should throw an error")
-      })
+    assert.fail("Should throw an error")
     })
   })
+  
   //-----------  Account key signs transactions depositing into account -----------
   // Require a receiving signature when creating account transaction
   describe('Account key signatures to deposit into account', function () {
@@ -209,13 +211,13 @@ describe('#createAccount()', function () {
         accountInfoFromConsensusNode.isReceiverSignatureRequired
 
       // query account via mirror node to confirm availability after creation
-      const respJSON = await getJsonData(accountIDFromConsensusNode)
+      const respJSON = await getJsonAccountData(accountIDFromConsensusNode)
       const recvdSignatureStatusFromMirrorNode = 
         respJSON.accounts[0].receiver_sig_required
   
       // confirm pass status for account creation with signature required
-      expect(Boolean(recvdSignatureStatusFromConsensusNode)).to.equal(true)
-      expect(Boolean(recvdSignatureStatusFromMirrorNode)).to.equal(true)
+      expect(recvdSignatureStatusFromConsensusNode).to.equal(true)
+      expect(recvdSignatureStatusFromMirrorNode).to.equal(true)
     })
     // Creates new account that doesn't require transactions to have receiving signature 
     it('Creates account without receiver signature required', async function () {
@@ -237,15 +239,16 @@ describe('#createAccount()', function () {
         accountInfoFromConsensusNode.isReceiverSignatureRequired
  
       // query account via mirror node to confirm availability after creation
-      const respJSON = await getJsonData(accountIDFromConsensusNode)
+      const respJSON = await getJsonAccountData(accountIDFromConsensusNode)
       const recvdSignatureStatusFromMirrorNode = 
         respJSON.accounts[0].receiver_sig_required
  
       // confirm pass for account creation with requirement for signature set to true
-      expect(Boolean(recvdSignatureStatusFromConsensusNode)).to.equal(false)
-      expect(Boolean(recvdSignatureStatusFromMirrorNode)).to.equal(false)
+      expect(recvdSignatureStatusFromConsensusNode).to.equal(false)
+      expect(recvdSignatureStatusFromMirrorNode).to.equal(false)
     })
   })
+
   //----------- Maximum number of tokens that an Account be associated with -----------
   describe('Max Token Association', function () {
     // Creates an account with a default max token association
@@ -284,32 +287,32 @@ describe('#createAccount()', function () {
     // Create an account with token association over the max
     it('Max token association over the maximum - should have status REQUESTED_NUM_AUTOMATIC_ASSOCIATIONS_EXCEEDS_ASSOCIATION_LIMIT', async function () {
       try {
-        await JSONRPCRequest('createAccount', {
+        const response = await JSONRPCRequest('createAccount', {
           publicKey: publicKey,
           initialBalance: 0,
           maxAutomaticTokenAssociations: 50000,
         })
+        if(response.status === "NOT_IMPLEMENTED") this.skip()
       } catch (err) {
         assert.equal(err.data.status, 'REQUESTED_NUM_AUTOMATIC_ASSOCIATIONS_EXCEEDS_ASSOCIATION_LIMIT');
       }
     })
     // Create an account with a max token association of -1
     it('Max token association of -1', async function () {
-      const response = await JSONRPCRequest('createAccount', {
-        publicKey: publicKey,
-        initialBalance: 0,
-        maxAutomaticTokenAssociations: -1,
-      })
-      if(response.status === "NOT_IMPLEMENTED") this.skip()
-      const newAccountId = response.accountId
-      // consensus node account
-      const accountInfoFromConsensusNode = await getAccountInfo(newAccountId)
-      const acctMaxTokenConsensus = 
-        accountInfoFromConsensusNode.maxAutomaticTokenAssociations.low
-
-      assert.equal(acctMaxTokenConsensus, -1)
+      try {
+        const response = await JSONRPCRequest('createAccount', {
+          publicKey: publicKey,
+          initialBalance: 0,
+          maxAutomaticTokenAssociations: -1,
+        })
+        if(response.status === "NOT_IMPLEMENTED") this.skip()
+      } catch(err) {
+        return
+      }   
+      assert.fail("Should throw an error")   
     })
   })
+
   //----------- Staked ID - ID of the account to which is staking --------------------
   describe('Staked ID, ID of account or node to which is staking', async function () {
     // Create an account and set staked account ID to operator account ID
@@ -329,7 +332,7 @@ describe('#createAccount()', function () {
         accountInfoFromConsensusNode.stakingInfo.stakedAccountId.toString()
 
       // query account via mirror node to confirm availability after creation
-      const respJSON = await getJsonData(accountID)
+      const respJSON = await getJsonAccountData(accountID)
       const stakedIDFromMirrorNode = respJSON.accounts[0].staked_account_id
 
       // confirm pass for account creation with a set staked account ID
@@ -339,31 +342,31 @@ describe('#createAccount()', function () {
       expect(stakedIDFromMirrorNode).to.equal(process.env.OPERATOR_ACCOUNT_ID)
     })
     // Create an account and set staked node ID and a node ID    
-      it('Creates an account and sets staked node ID to a node ID', async function () {
-        if(local) this.skip()
+    it('Creates an account and sets staked node ID to a node ID', async function () {
+      if(local) this.skip()
 
-        // select a staked node id between 0 and 6 for the test
-        const randomNodeId = Math.floor(Math.random() * 6) + 1
-        const response = await JSONRPCRequest('createAccount', {
-          publicKey: publicKey,
-          stakedNodeId: randomNodeId
-        })
-        if(response.status === "NOT_IMPLEMENTED") this.skip()
-        const newAccountId = response.accountId
+      // select a staked node id between 0 and 6 for the test
+      const randomNodeId = Math.floor(Math.random() * 6) + 1
+      const response = await JSONRPCRequest('createAccount', {
+        publicKey: publicKey,
+        stakedNodeId: randomNodeId
+      })
+      if(response.status === "NOT_IMPLEMENTED") this.skip()
+      const newAccountId = response.accountId
 
-        // query account via consensus node to verify creation
-        const accountInfoFromConsensusNode = await getAccountInfo(newAccountId)
-        const accountID = accountInfoFromConsensusNode.accountId.toString()
-        const stakedNodeIDFromConsensusNode =
-          accountInfoFromConsensusNode.stakingInfo.stakedNodeId.low.toString()
+      // query account via consensus node to verify creation
+      const accountInfoFromConsensusNode = await getAccountInfo(newAccountId)
+      const accountID = accountInfoFromConsensusNode.accountId.toString()
+      const stakedNodeIDFromConsensusNode =
+        accountInfoFromConsensusNode.stakingInfo.stakedNodeId.low.toString()
 
-        // query account via mirror node to confirm availability after creation
-        const respJSON = await getJsonData(accountID)
-        const stakedNodeIDFromMirrorNode = respJSON.accounts[0].staked_node_id
+      // query account via mirror node to confirm availability after creation
+      const respJSON = await getJsonAccountData(accountID)
+      const stakedNodeIDFromMirrorNode = respJSON.accounts[0].staked_node_id
 
-        // confirm pass for account creation with a set staked node ID
-        expect(Number(stakedNodeIDFromConsensusNode)).to.equal(randomNodeId)
-        expect(Number(stakedNodeIDFromMirrorNode)).to.equal(randomNodeId)
+      // confirm pass for account creation with a set staked node ID
+      expect(Number(stakedNodeIDFromConsensusNode)).to.equal(randomNodeId)
+      expect(Number(stakedNodeIDFromMirrorNode)).to.equal(randomNodeId)
     })
     // Create an account and set the staked account ID to an invalid ID
     it('Creates an account and sets the staked account ID to an invalid ID', async function () {
@@ -457,6 +460,7 @@ describe('#createAccount()', function () {
       }
     }) 
   })
+
   //----------- If true - account declines receiving a staking reward -----------
   describe('Account declines receiving a staking reward', async function () {
     // Create an account and set the account to decline staking rewards
@@ -473,12 +477,12 @@ describe('#createAccount()', function () {
       const cNodeRes = cNodeQuery.stakingInfo.declineStakingReward
 
       // Query the mirror node
-      const mNodeQuery = await getJsonData(newAccountID)
+      const mNodeQuery = await getJsonAccountData(newAccountID)
       const mNodeRes = mNodeQuery.accounts[0].decline_reward
 
       expect(cNodeRes).to.be.true
       expect(mNodeRes).to.be.true
-    })
+    })    
     // Create an account and leave decline rewards at default value
     it('Creates an account and leave staking rewards at default value', async function () {
       const response = await JSONRPCRequest('createAccount', {
@@ -491,7 +495,7 @@ describe('#createAccount()', function () {
       const cNodeRes = cNodeQuery.stakingInfo.declineStakingReward
 
       // Query the mirror node
-      const mNodeQuery = await getJsonData(newAccountID)
+      const mNodeQuery = await getJsonAccountData(newAccountID)
       const mNodeRes = mNodeQuery.accounts[0].decline_reward
 
       expect(cNodeRes).to.be.false
@@ -511,6 +515,7 @@ describe('#createAccount()', function () {
       assert.fail("Should throw an error")
     })
   })
+
   describe('Create accounts with a memo', async function () {
     // Create an account with a memo
     it('Creates an account with a memo', async function () {
@@ -528,7 +533,7 @@ describe('#createAccount()', function () {
       const cNodeRes = cNodeQuery.accountMemo
 
       // Query the mirror node
-      const mNodeQuery = await getJsonData(newAccountID)
+      const mNodeQuery = await getJsonAccountData(newAccountID)
       const mNodeRes = mNodeQuery.accounts[0].memo
 
       expect(cNodeRes).to.equal(testMemo)
@@ -550,8 +555,10 @@ describe('#createAccount()', function () {
       }
       assert.fail("Should throw an error")
     })
-    //----------- Set auto renew periods -----------
-    describe("Create account with specific auto renew period", async function () {
+  })
+
+  //----------- Set auto renew periods -----------
+  describe("Create account with specific auto renew period", async function () {
     // Create an account and set auto renew period to 2,592,000 seconds
     it("should set account auto renew period to 2,592,000 seconds", async function () {
       const response = await JSONRPCRequest('createAccount', {
@@ -567,32 +574,31 @@ describe('#createAccount()', function () {
     })
     // Create an account and set auto renew period to -1
     it("should set account auto renew period to -1 seconds - returns 'AUTORENEW_DURATION_NOT_IN_RANGE'", async function () {
-      try{
-      const response = await JSONRPCRequest('createAccount', {
-        publicKey: publicKey,
-        autoRenewPeriod: BigInt(-1).toString(),
-      })
-      if(response.status === "NOT_IMPLEMENTED") this.skip()
-      }
-      catch (err) {
+      try {
+        const response = await JSONRPCRequest('createAccount', {
+          publicKey: publicKey,
+          autoRenewPeriod: BigInt(-1).toString(),
+        })
+        if(response.status === "NOT_IMPLEMENTED") this.skip()
+      } catch(err) {
         assert.equal(err.data.status, 'AUTORENEW_DURATION_NOT_IN_RANGE')
-        }
+      }
     })
     // Create an account and set the auto renew period to 10 days (864000 seconds)
     it("should set account auto renew period to 864000 seconds returns 'AUTORENEW_DURATION_NOT_IN_RANGE'", async function () {
-      try{
-      const response = await JSONRPCRequest('createAccount', {
-        publicKey: publicKey,
-        autoRenewPeriod: BigInt(864000).toString(),
+      try {
+        const response = await JSONRPCRequest('createAccount', {
+          publicKey: publicKey,
+          autoRenewPeriod: BigInt(864000).toString(),
       })
       if(response.status === "NOT_IMPLEMENTED") this.skip()
       }
-      catch (err) {
-        assert.equal(err.data.status, 'AUTORENEW_DURATION_NOT_IN_RANGE')
-        }
+      catch(err) {
+      assert.equal(err.data.status, 'AUTORENEW_DURATION_NOT_IN_RANGE')
+      }
     })
    })
-  })  
+
   ///----------- Set account alias -----------
   describe("Create account with alias", async function () {
     // Create an account by using an alias
@@ -620,7 +626,7 @@ describe('#createAccount()', function () {
         accountInfoFromConsensusNode.accountId.toString()
 
       // query account via mirror node to confirm availability after creation
-      const accountInfoFromMirrorNode = await getJsonData(accountIDFromConsensusNode)
+      const accountInfoFromMirrorNode = await getJsonAccountData(accountIDFromConsensusNode)
       const accountAliasFromMirrorNode = 
         accountInfoFromMirrorNode.accounts[0].alias
       const accountMemoFromMirrorNode = 
@@ -629,7 +635,7 @@ describe('#createAccount()', function () {
       expect(accountInfoFromConsensusNode.aliasKey.toString()).to.equal(aliasPublicKey.toString()) 
       expect(accountInfoFromConsensusNode.accountMemo).to.equal('auto-created account') 
       expect(accountAliasFromMirrorNode).to.not.be.null
-      expect(accountMemoFromMirrorNode).to.equal('auto-created account')             
+      expect(accountMemoFromMirrorNode).to.equal('auto-created account')            
     })
   })
   
